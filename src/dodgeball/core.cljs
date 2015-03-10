7243f9fb9a94e548bc37db05657d2b0324b78d54
Bobby Wilson <bobbywilson0@gmail.com>
2015-03-09 20:15:34 -0600

    restructure data

--- src/dodgeball/core.cljs
+++ src/dodgeball/core.cljs
@@ -5,37 +5,49 @@
 (enable-console-print!)
 
 
-(defonce app-state (atom {:balls [1 0 1 0 1 0 1 0 1]}))
+(defonce app-state (atom
+                    {:team1 [{:x 0, :y 0, :has-ball false}
+                             {:x 2, :y 0, :has-ball false}
+                             {:x 4, :y 0, :has-ball false}
+                             {:x 6, :y 0, :has-ball false}
+                             {:x 8, :y 0, :has-ball false}]
+                     :team2 [{:x 0, :y 8, :has-ball false}
+                             {:x 2, :y 8, :has-ball false}
+                             {:x 4, :y 8, :has-ball false}
+                             {:x 6, :y 8, :has-ball false}
+                             {:x 8, :y 8, :has-ball false}]
+                     :loose-balls [{:x 0, :y 4}
+                                   {:x 2, :y 4}
+                                   {:x 4, :y 4}
+                                   {:x 6, :y 4}
+                                   {:x 8, :y 4}]}))
+
+(def board-width 9)
+(def board-height 9)
+(def bench-size 4)
 
 (defn bench []
   (dom/table nil
     (dom/tr nil
-      (vec (for [n (range 0 4)]
+      (vec (for [n (range 0 bench-size)]
        (dom/td nil))))))
 
 
 (defn cell-view [n owner]
   (om/component (dom/td nil n)))
 
-(defn row-view [data owner]
-  (om/component
-    (apply dom/tr nil
-      (om/build-all cell-view data))))
 
 (defn board [data]
  (dom/div #js {:className "container"}
  (bench)
  (dom/table nil
-    (vec (for [y (range 0 9)]
-      (if (= y 5)
-        (om/build row-view (:balls data))
-
-        (dom/tr
-          #js {:className
-            (cond
-              (or (= y 3) (= y 6)) "middle-top")}
-          (vec (for [x (range 0 9)]
-            (dom/td nil y))))))))
+    (vec (for [y (range 0 board-height)]
+      (dom/tr
+        #js {:className
+          (cond
+            (or (= y 3) (= y 6)) "middle-top")}
+        (vec (for [x (range 0 board-width)]
+          (om/build cell-view y)))))))
   (bench)))
 
 
