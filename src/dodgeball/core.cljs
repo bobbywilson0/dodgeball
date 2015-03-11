84c32d3dba3ef2bb95974f9f9ed032eef529f2d9
Bobby Wilson <bobbywilson0@gmail.com>
2015-03-11 17:23:18 -0600

    updated character placement

--- src/dodgeball/core.cljs
+++ src/dodgeball/core.cljs
@@ -7,25 +7,33 @@
 
 (defonce app-state (atom
                     {:cells
-                      [{:x 0, :y 0, :team \a, :image "back-noball.gif"}
-                       {:x 2, :y 0, :team \a, :image "back-noball.gif"}
-                       {:x 4, :y 0, :team \a, :image "back-noball.gif"}
-                       {:x 6, :y 0, :team \a, :image "back-noball.gif"}
-                       {:x 8, :y 0, :team \a, :image "back-noball.gif"}
-                       {:x 0, :y 8, :team \b, :image "back-noball.gif"}
-                       {:x 2, :y 8, :team \b, :image "back-noball.gif"}
-                       {:x 4, :y 8, :team \b, :image "back-noball.gif"}
-                       {:x 6, :y 8, :team \b, :image "back-noball.gif"}
-                       {:x 8, :y 8, :team \b, :image "back-noball.gif"}
-                       {:x 0, :y 4, :ball true, :image "ball.gif"}
-                       {:x 2, :y 4, :ball true, :image "ball.gif"}
-                       {:x 4, :y 4, :ball true, :image "ball.gif"}
-                       {:x 6, :y 4, :ball true, :image "ball.gif"}
-                       {:x 8, :y 4, :ball true, :image "ball.gif"}]}))
+                      {:team-a
+                         [{:player 1 :x 0, :y 0}
+                          {:player 2 :x 2, :y 0}
+                          {:player 3 :x 4, :y 0}
+                          {:player 4 :x 6, :y 0}
+                          {:player 5 :x 8, :y 0}]
+                       :team-b
+                         [{:player 1 :x 0, :y 8}
+                          {:player 2 :x 2, :y 8}
+                          {:player 3 :x 4, :y 8}
+                          {:player 4 :x 6, :y 8}
+                          {:player 5 :x 8, :y 8}]
+                       :loose-balls
+                         [{:x 0, :y 4}
+                         {:x 2, :y 4}
+                         {:x 4, :y 4}
+                         {:x 6, :y 4}
+                         {:x 8, :y 4}]
+                     :selected-unit {}}}))
 
 (def board-width 9)
 (def board-height 9)
 (def bench-size 4)
+(def player-image "images/back-noball.gif")
+(def ball-image "images/ball.gif")
+
+
 
 (defn bench []
   (dom/table nil
@@ -34,10 +42,18 @@
        (dom/td nil))))))
 
 
-(defn cell-view [data owner]
+(defn player [data owner]
+  (do (println data))
+  (if data
+    (om/component (dom/td #js {:onClick #(om/set-state! owner :selected-unit data) }
+                          (dom/img #js {:src player-image })))
+    (om/component (dom/td #js {:onClick #(println data) }
+                          ))))
+
+(defn ball [data owner]
   (om/component (dom/td nil
                         (if data
-                          (dom/img #js {:src (str "images/" (:image data))})))))
+                          (dom/img #js {:src ball-image})))))
 
 (defn find-cell [x y data]
   (first
@@ -55,10 +71,13 @@
         #js {:className
           (cond
             (or (= y 3) (= y 6)) "middle-top")}
-        (vec (for [x (range 0 board-width)]
-          (om/build cell-view
-                    (find-cell x y (:cells data))))))))
-  (bench))))
+              (vec (for [x (range 0 board-width)]
+                (if (= y 4)
+                  (om/build ball
+                    (find-cell x y (concat [:x x] [:y y] (:loose-balls (:cells data)))))
+                  (om/build player
+                    (concat (:team-a (:cells data)) (:team-b (:cells data)))))))))))
+  (bench)))
 
 
 (om/root
