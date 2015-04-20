683895da10e0af2ea4cc7836f059d78571924fba
Bobby Wilson <bobbywilson0@gmail.com>
2015-04-19 22:37:56 -0600

    remove duplication in actions, remove coords key from returned map

--- src/cljs/dodgeball/actions.cljs
+++ src/cljs/dodgeball/actions.cljs
@@ -8,19 +8,14 @@
 
 (defn switch-turns []
   (if (= (:turn @state/game) :red)
-    (do
-      (swap! state/game assoc :turn :blue)
-      (swap! state/game assoc :actions 0))
-    (do
-      (swap! state/game assoc :turn :red)
-      (swap! state/game assoc :actions 0))))
+      (swap! state/game assoc :turn :blue :actions 0)
+      (swap! state/game assoc :turn :red :actions 0)))
 
 (defn handle-event [ch]
   (go
      (loop []
        (let [event (<! ch)
-             x (:x (:coords event))
-             y (:y (:coords event))]
+             {:keys [x y]} event]
          (println event)
           (case (:type event)
             :select-unit   (unit/select-unit (:id event))
@@ -28,18 +23,16 @@
             :move-unit     (do
                              (unit/move-unit x y)
                              (swap! state/game update-in [:actions] inc)
-                             (unit/deselect-unit)
-                             (if (= (:actions @state/game) 2) (switch-turns)))
+                             (unit/deselect-unit))
             :pickup-ball   (do
                              (unit/pickup-ball x y)
                              (swap! state/game update-in [:actions] inc)
-                             (unit/deselect-unit)
-                             (if (= (:actions @state/game) 2) (switch-turns)))
+                             (unit/deselect-unit))
             :attack        (do
                              (combat/attack x y)
                              (swap! state/game update-in [:actions] inc)
-                             (unit/deselect-unit)
-                             (if (= (:actions @state/game) 2) (switch-turns))))
+                             (unit/deselect-unit)))
+         (if (= (:actions @state/game) 2) (switch-turns))
          (recur)))))
 
 (defn determine-action [x y]
@@ -52,36 +45,41 @@
       (boolean unit))
      {:type   :select-unit
       :id     (:id unit)
-      :coords {:x x :y y}}
+      :x      x
+      :y      y}
 
      (and
        (boolean (state/selected-unit))
        (boolean (unit/find-one-unit-by x y (state/defense))))
      {:type :attack
-      :id (:id unit)
-      :coords {:x x :y y}}
+      :id   (:id unit)
+      :x    x
+      :y    y}
      (or
        (boolean unit)
        (not (unit/in-movement-range? x y (state/selected-unit)))
        (and
          (= nil (state/selected-unit))
          (boolean (unit/find-one-unit-by x y (state/defense)))))
-     {:type   :deselect-unit
-      :id     (:id unit)
-      :coords {:x x :y y}}
+     {:type :deselect-unit
+      :id   (:id unit)
+      :x    x
+      :y    y}
 
      (and
        (boolean (unit/find-one-unit-by x y :ball))
        (boolean (state/selected-unit)))
-     {:type   :pickup-ball
-      :id     (:id (unit/find-one-unit-by x y :ball))
-      :coords {:x x :y y}}
+     {:type :pickup-ball
+      :id   (:id (unit/find-one-unit-by x y :ball))
+      :x    x
+      :y    y}
 
 
      (unit/in-movement-range? x y (state/selected-unit))
      {:type :move-unit
-      :id (:id (state/selected-unit))
-      :coords {:x x :y y}}
+      :id   (:id (state/selected-unit))
+      :x    x
+      :y    y}
 
      :else
        (println x y (:turn @state/game)))))
