7ecbdb9f0272040800829d372bb34033cd4d0778
Bobby Wilson <bobbywilson0@gmail.com>
2015-04-24 19:53:44 -0600

    figwheel and testing working

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
             :description "FIXME: write description"
             :url "http://example.com/FIXME"
             :license {:name "Eclipse Public License"
-                      :url  "http://www.eclipse.org/legal/epl-v10.html"}
+                      :url "http://www.eclipse.org/legal/epl-v10.html"}
 
             :source-paths ["src/clj" "src/cljs"]
 
@@ -10,24 +10,23 @@
                            [ring-server "0.4.0"]
                            [cljsjs/react "0.13.1-0"]
                            [reagent "0.5.0"]
-                           [reagent-forms "0.4.6"]
+                           [reagent-forms "0.5.0"]
                            [reagent-utils "0.1.4"]
                            [org.clojure/clojurescript "0.0-3196" :scope "provided"]
-                           [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                            [ring "1.3.2"]
                            [ring/ring-defaults "0.1.4"]
                            [prone "0.8.1"]
-                           [compojure "1.3.2"]
+                           [compojure "1.3.3"]
                            [selmer "0.8.2"]
                            [environ "1.0.0"]
-                           [secretary "1.2.2"]]
+                           [secretary "1.2.3"]]
 
             :plugins [[lein-cljsbuild "1.0.4"]
                       [lein-environ "1.0.0"]
                       [lein-ring "0.9.1"]
                       [lein-asset-minifier "0.2.2"]]
 
-            :ring {:handler      dodgeball.handler/app
+            :ring {:handler dodgeball.handler/app
                    :uberwar-name "dodgeball.war"}
 
             :min-lein-version "2.5.0"
@@ -38,68 +37,64 @@
 
             :clean-targets ^{:protect false} ["resources/public/js"]
 
-
-
             :minify-assets
             {:assets
              {"resources/public/css/site.min.css" "resources/public/css/site.css"}}
 
-            :cljsbuild {:builds [{:id             "slimer"
-                                  :source-paths   ["src/cljs" "test"]
-                                  :notify-command ["slimerjs"
-                                                   "vendor/test/unit-test.js"
-                                                   "target/testable.js"]
-                                  :compiler       {:output-to      "target/testable.js"
-                                                   :optimizations  :whitespace
-                                                   :cache-analysis false
-                                                   :pretty-print   true}}
-                                 {:id             "phantom"
-                                  :source-paths   ["src/cljs" "test"]
-                                  :notify-command ["phantomjs"
-                                                   "vendor/test/unit-test.js"
-                                                   "vendor/test/phantomjs-shims.js"
-                                                   "target/testable.js"]
-                                  :compiler       {:output-to      "target/testable.js"
-                                                   :optimizations  :whitespace
-                                                   :cache-analysis false
-                                                   :pretty-print   true}}]}
+            :cljsbuild {:builds {:app {:source-paths ["src/cljs"]
+                                       :compiler {:output-to     "resources/public/js/app.js"
+                                                  :output-dir    "resources/public/js/out"
+                                                  :asset-path   "js/out"
+                                                  :optimizations :none
+                                                  :pretty-print  true}}}}
 
-            :profiles {:dev {:repl-options {:init-ns          dodgeball.repl
+            :profiles {:dev {:repl-options {:init-ns dodgeball.repl
                                             :nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
 
                              :dependencies [[ring-mock "0.1.5"]
                                             [ring/ring-devel "1.3.2"]
                                             [leiningen "2.5.1"]
-                                            [figwheel "0.2.5"]
+                                            [figwheel "0.2.6"]
                                             [weasel "0.6.0"]
-                                            [com.cemerick/piggieback "0.1.6-SNAPSHOT"]
+                                            [com.cemerick/piggieback "0.2.0"]
+                                            [org.clojure/tools.nrepl "0.2.10"]
                                             [pjstadig/humane-test-output "0.7.0"]]
 
                              :source-paths ["env/dev/clj"]
-                             :plugins      [[lein-figwheel "0.2.3-SNAPSHOT"]]
+                             :plugins [[lein-figwheel "0.2.5"]
+                                       [com.cemerick/clojurescript.test "0.3.2"]]
 
-                             :injections   [(require 'pjstadig.humane-test-output)
-                                            (pjstadig.humane-test-output/activate!)]
+                             :injections [(require 'pjstadig.humane-test-output)
+                                          (pjstadig.humane-test-output/activate!)]
 
-                             :figwheel     {:http-server-root "public"
-                                            :server-port      3449
-                                            :css-dirs         ["resources/public/css"]
-                                            :ring-handler     dodgeball.handler/app}
+                             :figwheel {:http-server-root "public"
+                                        :server-port 3449
+                                        :css-dirs ["resources/public/css"]
+                                        :ring-handler dodgeball.handler/app}
 
-                             :env          {:dev? true}
+                             :env {:dev? true}
 
-                             :cljsbuild {:builds [{:app {:source-paths ["env/dev/cljs"]
-                                                         :compiler     {:main       "dodgeball.dev"
-                                                                        :source-map true}}}]}
+                             :cljsbuild {:builds {:app {:source-paths ["env/dev/cljs"]
+                                                        :compiler {:main "dodgeball.dev"
+                                                                   :source-map true}}
+                                                  :test {:source-paths ["src/cljs"  "test/cljs"]
+                                                         :compiler {:output-to "target/test.js"
+                                                                    :optimizations :whitespace
+                                                                    :pretty-print true
+                                                                    :preamble ["react/react.js"]}}}
+                                         :test-commands {"unit" ["phantomjs" :runner
+                                                                 "test/vendor/es5-shim.js"
+                                                                 "test/vendor/es5-sham.js"
+                                                                 "test/vendor/console-polyfill.js"
+                                                                 "target/test.js"]}}}
 
-                             :uberjar      {:hooks       [leiningen.cljsbuild minify-assets.plugin/hooks]
-                                            :env         {:production true}
-                                            :aot         :all
-                                            :omit-source true
-                                            :cljsbuild   {:jar    true
-                                                          :builds {:app
-                                                                   {:source-paths ["env/prod/cljs"]
-                                                                    :compiler
-                                                                                  {:optimizations :advanced
-                                                                                   :pretty-print  false}}}}}}}
-            )
+                       :uberjar {:hooks [leiningen.cljsbuild minify-assets.plugin/hooks]
+                                 :env {:production true}
+                                 :aot :all
+                                 :omit-source true
+                                 :cljsbuild {:jar true
+                                             :builds {:app
+                                                      {:source-paths ["env/prod/cljs"]
+                                                       :compiler
+                                                                     {:optimizations :advanced
+                                                                      :pretty-print false}}}}}})
